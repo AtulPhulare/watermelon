74a55cb03c6b236056469bdd6d44bde9cbd5ad8a
Atulphulare <atulm.fulare786@gmail.com>
2024-12-21 23:40:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('.transparent-header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+// Smooth scroll for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
